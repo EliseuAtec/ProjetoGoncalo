b8a59ec22200aef68ed96ad904a35c93d0755f02
RubenAtec <ruben.alves.t0123774@edu.atec.pt>
2023-06-28 14:07:04 +0100

    update

--- sistema_bd/function.js
+++ sistema_bd/function.js
@@ -1,20 +1,173 @@
+
 function btnCriarNomeTabela() {
-    const inputNametable = document.getElementById('inputNametable').value;
+  var aparecerCamposEstrutura = document.getElementById('columnsDiv');
+  
+  aparecerCamposEstrutura.innerHTML = `
+    <div class="container">
+      <div class="row mt-3">
+        <div class="col-3">
+          <label for="table-name-input" class="form-label">NOME DA TABELA:</label>
+          <input type="text" class="form-control" id="table-name-input">
+        </div>
+        <button type="button" class="btn btn-primary" onclick="criarTabela()" style="float: right;">CRIAR</button>
+      </div>
+    </div>
+  `;
+}
 
-    if (inputNametable.trim() === '') {
-        alert('Por favor, insira um nome para a tabela!');
-        return;
+function criarTabela() {
+  var inputNametable = document.getElementById('table-name-input');
+  var tableName = inputNametable.value.trim();
+  if (tableName === '') {
+    alert('Por favor, insira um nome para a tabela!');
+    return;
+  }
+
+  fetch('http://localhost:3000/create-table', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ tableName: tableName })
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+      inputNametable.value = ''; // Limpar o campo de entrada do nome da tabela
 
-    // Mostra os campos da estrutura da tabela criada
-    document.getElementById('aparecerCamposEstrutura').style.display = 'block';
-        
+      // Adicionar o link na navbar
+      var navBar = document.getElementById('navbarNav').getElementsByTagName('ul')[0];
+      var newListItem = document.createElement('li');
+      newListItem.className = 'nav-item';
+
+      var newLink = document.createElement('a');
+      newLink.className = 'nav-link';
+      newLink.href = '#' + tableName;
+      newLink.textContent = tableName;
+
+      newListItem.appendChild(newLink);
+      navBar.appendChild(newListItem);
+    })
+    .catch((error) => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
 }
 
 
+// Carrega as colunas da tabela
+function displayTableColumns(tableName) {
+  console.log('About to fetch table columns for:', tableName);
+  fetch(`http://localhost:3000/get-table-columns/${tableName}`, {
+    method: 'GET',
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Fetched table columns:', data);
+      var columnsDiv = document.getElementById('columnsDiv');
+      console.log('columnsDiv element:', columnsDiv);
+      // Limpar o columnsDiv antes de adicionar novos parágrafos
+      columnsDiv.innerHTML = '';
+
+      // Adiciona o formulário HTML
+      columnsDiv.insertAdjacentHTML('beforeend', `
+        <div class="container">
+          <div class="row mt-3">
+            <div class="col-3">
+              <label for="inputName" class="form-label">Name</label>
+              <input type="text" class="form-control" id="inputName">
+            </div>
+            <div class="col-3">
+              <label for="inputType" class="form-label">Type</label>
+              <select class="form-control" id="inputType">
+                <option value="">---</option>
+                <option value="INT">INT</option>
+                <option value="VARCHAR">VARCHAR</option>
+                <option value="TEXT">TEXT</option>
+                <option value="DATE">DATE</option>
+              </select>
+            </div>
+            <div class="col-3">
+              <label for="inputValue" class="form-label">Length/Values</label>
+              <input type="text" class="form-control" id="inputValue">
+            </div>
+            <div class="col-3">
+              <label for="inputIndex" class="form-label">Index</label>
+              <select class="form-control" id="inputIndex">
+                <option value="">---</option>
+                <option value="PRIMARY">PRIMARY</option>
+                <option value="UNIQUE">UNIQUE</option>
+                <option value="INDEX">INDEX</option>
+                <option value="FULLTEXT">FULLTEXT</option>
+                <option value="SPATIAL">SPATIAL</option>
+              </select>
+            </div>
+          </div>
+          <button type="button" class="btn btn-primary" style="float: right;">CRIAR</button>
+        </div>
+      `);
+
+      // Use data.columns em vez de apenas data
+      data.columns.forEach(column_name => {
+        var newParagraph = document.createElement('p');
+        newParagraph.textContent = column_name;
+        columnsDiv.appendChild(newParagraph);
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
 
 
+// Recarrega a página ao clicar em um link da navbar
+function reloadPage(tableName) {
+  location.reload();
+}
 
+window.onload = function() {
+  fetch('http://localhost:3000/get-tables', {
+    method: 'GET',
+  })
+    .then(response => response.json())
+    .then(data => {
+      var navBar = document.getElementsByClassName('navbar-nav')[0];
+      data.tables.forEach(table_name => {
+        var newListItem = document.createElement('li');
+        newListItem.className = "nav-item";
 
+        var newLink = document.createElement('a');
+        newLink.className = "nav-link";
+        newLink.id = table_name;
+        newLink.href = "#" + table_name;
+        newLink.textContent = table_name;
 
+        newLink.onclick = function() {
+          // Remove a classe 'active' de todos os links da navbar
+          var navLinks = document.getElementsByClassName('nav-link');
+          for (var i = 0; i < navLinks.length; i++) {
+            navLinks[i].classList.remove('active');
+          }
+          // Adiciona a classe 'active' ao link clicado
+          this.classList.add('active');
 
+          // Limpa o conteúdo do columnsDiv
+          var columnsDiv = document.getElementById('columnsDiv');
+          columnsDiv.innerHTML = '';
+
+          // Exibe o conteúdo das colunas da tabela
+          displayTableColumns(table_name);
+        };
+
+        newListItem.appendChild(newLink);
+        navBar.appendChild(newListItem);
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
--- sistema_bd/index.js
+++ sistema_bd/index.js
@@ -1,53 +1,68 @@
-//https://www.youtube.com/watch?v=w0raNWk2ybg
-
-
-
-const mysql = require('mysql');   //importar o pacote "mysql"
 const express = require('express');
-var app = express()
+const mysql = require('mysql');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const port = 3000
+const app = express();
+const port = 3000;
+
+app.use(cors());
 app.use(bodyParser.json());
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'db',
+});
 
-const connection = mysql.createConnection({  //conexão com o banco de dados MySQL usando as informações apropriadas, como host, porta, nome do usuário, senha e nome do banco de dados
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: "db"  //nome da base de dados.. create datebase
-  });
+connection.connect((error) => {
+  if (error) {
+    console.error('An error occurred while connecting to the DB:', error);
+    return;
+  }
+  console.log('Successfully connected to the database!');
+});
 
 
-connection.connect((error) => { //Abrindo uma conexão 
+//Cria a tabela
+app.post('/create-table', (req, res) => {
+  const { tableName } = req.body;
+  const query = `CREATE TABLE ${tableName} (id INT)`;
+
+  connection.query(query, (error, results) => {
     if (error) {
-      console.error(error.code);
-      console.error(error.fatal);
-    } else {
-      console.log('\nConexão bem-sucedida ao banco de dados!');
+      console.error('An error occurred while creating the table:', error);
+      res.status(500).send('An error occurred while creating the table.');
+      return;
     }
+    console.log(`Successfully created table ${tableName}!`);
+    res.send(`Successfully created table ${tableName}!`);
   });
+});
 
-  $query = 'SELECT * FROM tabelateste';  //onde esta 'tabelateste' tem que puxar o nome da TABELA criada pelo usuario...
-  // $query = definir aqui se quer criar tabela, inserir dados... ;  
-  //$queryCriarTabela = 'CREATE TABLE `db`.`tabelateste2` (`Produto` VARCHAR(100) NOT NULL , UNIQUE (`Produto`)) ENGINE = InnoDB;';
-//$queryCriarTabela = 'CREATE TABLE `db`.`tabelateste2` (`inputName` inputType(inputValue) NOT NULL , inputIndex (`inputName`)) ENGINE = InnoDB;';
+//Obtem nomes das tabela para a navbar
+app.get('/get-tables', function (req, res) {
+  connection.query('SHOW TABLES', function (error, results, fields) {
+      if (error) throw error;
 
-  //mostra o resultado da consulta
-  connection.query($query, (error, results) => {
-    if (error) {
-      console.error('Erro ao executar a consulta: ', error);  //caso surgir erro para consultar os dados
-    } else {
-      console.log('\nResultado: ', results);  //se for sucesso, mostra os resultados..
-    }
+      // 'results' é um array de objetos onde cada objeto tem uma propriedade que corresponde ao nome da tabela
+      // Vamos extrair os nomes das tabelas para um array
+      var tables = results.map(row => row[Object.keys(row)[0]]);
+
+      // Retornar o array de nomes das tabelas como um objeto JSON
+      res.json({ tables: tables });
   });
-  
-  connection.end(function() {  //finalizar conexao com a base de dados...
-    console.log("\nFunção finalizada!");
-  })
-  
-  // iniciar o servidor
-  app.listen(port, () => {
-    console.log(`Servidor iniciado na porta ${port}`)
-  })
+});
 
+app.get('/get-table-columns/:tableName', function (req, res) {
+  const { tableName } = req.params;
+  connection.query(`SHOW COLUMNS FROM ${tableName}`, function (error, results) {
+      if (error) throw error;
+      res.json({ columns: results.map(row => row.Field) });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
